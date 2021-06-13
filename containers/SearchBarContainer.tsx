c3657ec3196309659f7e6045d3946a4e19ed9ed3
noki <novak.kirill@gmail.com>
2021-06-13 13:15:56 +0300

    added icons

--- containers/SearchBarContainer.tsx
+++ containers/SearchBarContainer.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import {View, TextInput, Button, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const SearchBarContainer =  ()=>  {
 
     return (
         <View style={styles.main_container}>
-            <TextInput
+            <View style={styles.input_container}>
+                <TextInput
             styles={styles.input}
             placeholder="Search"
             />
+            </View>
+            
+        {/* <FontAwesome5 name="book" size={30} color="#4F8EF7" /> */}
+        <View style={styles.icon_container}>
+           <Icon name="times" size={20} color="#323131" /> 
+        </View>
+        
         </View>
     )
 
@@ -26,11 +35,20 @@ const styles = StyleSheet.create({
         borderColor: "lightskyblue",
         borderRadius:5,
         marginTop: 25,
-        marginBottom:25
+        marginBottom:25,
+        flexDirection: "row",
     },
     input: {
         paddingLeft:15,
         fontSize:25,
 
+    },
+    icon_container:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    input_container:{
+        flex:9,
     }
 })
